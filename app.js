d41a97a9e686333cd05e10ad14be2b47957334ed
mostofa zaman <mostofazaman1234@gmail.com>
2026-06-16 23:53:07 +0600

    Practice handling routes using req.url

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-
-  console.log(req.url);
-
- 
-  console.log(req.method);
-
   res.writeHead(200, {
     "Content-Type": "text/html",
   });
 
-  res.end("<h1>Hello</h1>");
+  if (req.url === "/") {
+    res.end("<h1>Home Page</h1>");
+  } else if (req.url === "/about") {
+    res.end("<h1>About Page</h1>");
+  } else {
+    res.writeHead(404);
+    res.end("<h1>Page Not Found</h1>");
+  }
 });
 
-server.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+server.listen(5000);
