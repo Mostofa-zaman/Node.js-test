97c6c274f717f579d2be5983e1630d9710c691c3
mostofa zaman <mostofazaman1234@gmail.com>
2026-06-16 23:54:41 +0600

    Practice basic route and method matching

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/html",
-  });
-
-  if (req.url === "/") {
-    res.end("<h1>Home Page</h1>");
-  } else if (req.url === "/about") {
-    res.end("<h1>About Page</h1>");
+  if (req.url === "/" && req.method === "GET") {
+    res.end("Home");
+  } else if (req.url === "/about" && req.method === "GET") {
+    res.end("About");
   } else {
     res.writeHead(404);
-    res.end("<h1>Page Not Found</h1>");
+    res.end("404 Not Found!");
   }
 });
 
-server.listen(5000);
+server.listen(5000, () => {
+  console.log("Server is running...");
+});
