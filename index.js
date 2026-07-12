662f2363bbaae6ffb90f6af6251c138003bf6d59
mostofa zaman <mostofazaman1234@gmail.com>
2026-07-12 23:38:39 +0600

    learn(express): create basic GET route

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
 const express = require("express");
-
 const app = express();
-
 app.use(express.json());
 
-// Registration
-app.post("/registration", (req, res) => {
-  const { username, email, password } = req.body;
 
-  if (!username) {
-    return res.send("Username is required");
-  }
 
-  if (!email) {
-    return res.send("Email is required");
-  }
+// // Registration
+// app.post("/registration", (req, res) => {
+//   const { username, email, password } = req.body;
 
-  if (!password) {
-    return res.send("Password is required");
-  }
+//   if (!username) {
+//     return res.send("Username is required");
+//   }
 
-  return res.send("Registration successful");
-});
+//   if (!email) {
+//     return res.send("Email is required");
+//   }
 
-// Login
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
+//   if (!password) {
+//     return res.send("Password is required");
+//   }
 
-  if (!email) {
-    return res.send("Email is required");
-  }
+//   return res.send("Registration successful");
+// });
 
-  if (!password) {
-    return res.send("Password is required");
-  }
+// // Login
+// app.post("/login", (req, res) => {
+//   const { email, password } = req.body;
 
-  return res.send("Login successful");
-});
+//   if (!email) {
+//     return res.send("Email is required");
+//   }
+
+//   if (!password) {
+//     return res.send("Password is required");
+//   }
+
+//   return res.send("Login successful");
+// });
+
+app.get("/bankamount", (req,res)=>{
+  console.log("first")
+})
+
 
 app.listen(5000, () => {
   console.log("Server is running on http://localhost:5000");
