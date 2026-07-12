38417f04be504595bb9477ed4ebd250d1934e725
mostofa zaman <mostofazaman1234@gmail.com>
2026-07-12 23:35:13 +0600

    learn(express): add basic login route

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-const express = require("express")
-const app = express()
+const express = require("express");
 
-app.use(express.json())
+const app = express();
 
+app.use(express.json());
 
- app.post("/registration", (req, res)=>{
-   const {username ,email, password}= req.body
+// Registration
+app.post("/registration", (req, res) => {
+  const { username, email, password } = req.body;
 
-   if(!username){
-   res.send( "username is required")
-    }
-   if(!email){
-    res.send( "email is required")
+  if (!username) {
+    return res.send("Username is required");
   }
-   if(!password){
-   res.send( "password is required")
-    } })
 
+  if (!email) {
+    return res.send("Email is required");
+  }
+
+  if (!password) {
+    return res.send("Password is required");
+  }
+
+  return res.send("Registration successful");
+});
+
+// Login
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.send("Email is required");
+  }
+
+  if (!password) {
+    return res.send("Password is required");
+  }
+
+  return res.send("Login successful");
+});
 
 app.listen(5000, () => {
-  console.log("Server is running...");
+  console.log("Server is running on http://localhost:5000");
 });
